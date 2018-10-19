d97d6a2481c8da72e02284ec42ae51a43e753596
shenyw <shenyw@citycloud.com.cn>
2018-10-19 17:00:13 +0800

    日志、当前登陆用户注解

--- src/main/java/com/cucci/common/annotions/CurrentUser.java
+++ src/main/java/com/cucci/common/annotions/CurrentUser.java
@@ -1,4 +1,9 @@
 package com.cucci.common.annotions;
 
+import java.lang.annotation.*;
+
+@Target(ElementType.METHOD)
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
 public @interface CurrentUser {
 }
