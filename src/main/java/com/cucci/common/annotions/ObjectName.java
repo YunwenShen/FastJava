c3b71293b5f57a0c146dfe7db29ba4fb05ddf3b2
shenyw <shenyw@citycloud.com.cn>
2018-10-19 17:49:17 +0800

    对象名称

---
+++ src/main/java/com/cucci/common/annotions/ObjectName.java
@@ -0,0 +1,9 @@
+package com.cucci.common.annotions;
+
+import java.lang.annotation.*;
+
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+public @interface ObjectName {
+}
