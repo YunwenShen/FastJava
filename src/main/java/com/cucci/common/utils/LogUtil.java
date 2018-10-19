73b39b3ea0fe6267563741ac8f5d0d1d4a29a9ae
shenyw <shenyw@citycloud.com.cn>
2018-10-19 17:53:10 +0800

    对象名称

--- src/main/java/com/cucci/common/utils/LogUtil.java
+++ src/main/java/com/cucci/common/utils/LogUtil.java
@@ -7,4 +7,8 @@ package com.cucci.common.utils;
  **/
 public class LogUtil {
 
+    public static <T> String compareEach(Object o1, Object o2, Class<T> clazz) {
+        // TODO 通过反射的方式对比 对象之间的区别
+        return null;
+    }
 }
