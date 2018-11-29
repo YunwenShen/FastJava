9b0890bf2c111ee0d51484444ccd9afebb44da10
shenyw <shenyw@citycloud.com.cn>
2018-11-29 18:07:01 +0800

    chore: 添加redis依赖、修改redis配置

---
+++ src/main/java/com/cucci/common/config/RedisConfig.java
@@ -0,0 +1,38 @@
+package com.cucci.common.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cache.annotation.CachingConfigurerSupport;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import redis.clients.jedis.JedisPool;
+import redis.clients.jedis.JedisPoolConfig;
+
+/**
+ * redis配置类
+ *
+ * @author shenyw
+ **/
+@Configuration
+@EnableCaching
+public class RedisConfig extends CachingConfigurerSupport {
+
+    @Value("${spring.redis.host}")
+    private String host;
+    @Value("${spring.redis.port}")
+    private int port;
+    @Value("${spring.redis.timeout}")
+    private int timeout;
+    @Value("${spring.redis.pool.max-idle}")
+    private int maxIdle;
+    @Value("${spring.redis.pool.max-wait}")
+    private long maxWaitMillis;
+
+    @Bean
+    public JedisPool redisPoolFactory() {
+        JedisPoolConfig jedisPoolConfig = new JedisPoolConfig();
+        jedisPoolConfig.setMaxIdle(maxIdle);
+        jedisPoolConfig.setMaxWaitMillis(maxWaitMillis);
+        return new JedisPool(jedisPoolConfig, host, port, timeout);
+    }
+}
---
+++ src/main/java/com/cucci/common/constants/CacheConstant.java
@@ -0,0 +1,19 @@
+package com.cucci.common.constants;
+
+/**
+ * 缓存相关常量
+ *
+ * @author shenyw
+ **/
+public final class CacheConstant {
+
+    /**
+     * redis默认过期时间
+     */
+    public final static int DEFAULT_EXPIRE = 3600;
+
+    /**
+     * 缓存类型和缓存key分隔符
+     */
+    public final static String TYPE_KEY_SEPARATOR = "-";
+}
