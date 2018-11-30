dae09fc79e7c31819f4a01f4fb4a5e612517891c
shenyw <shenyw@citycloud.com.cn>
2018-11-30 16:09:55 +0800

    feat：深拷贝数组

---
+++ src/main/java/com/cucci/common/utils/CollectionUtil.java
@@ -0,0 +1,31 @@
+package com.cucci.common.utils;
+
+import java.io.*;
+import java.util.List;
+
+/**
+ * 集合类工具类
+ *
+ * @author shenyw
+ **/
+public class CollectionUtil {
+
+    /**
+     * 深拷贝
+     *
+     * @param list 数组列表
+     * @param <T>  数组类型
+     * @return 拷贝结果
+     * @throws IOException
+     * @throws ClassNotFoundException
+     */
+    public static <T> T deepCopy(List<T> list) throws IOException, ClassNotFoundException {
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        ObjectOutputStream objectOutputStream = new ObjectOutputStream(outputStream);
+        objectOutputStream.writeObject(list);
+        //从流中取出
+        ByteArrayInputStream inputStream = new ByteArrayInputStream(outputStream.toByteArray());
+        ObjectInputStream objectInputStream = new ObjectInputStream(inputStream);
+        return (T) objectInputStream.readObject();
+    }
+}
