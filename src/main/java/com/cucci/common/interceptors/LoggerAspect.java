9f2f5b9b5cc3d6bdb7c3a5738fe23127fbf707f0
shenyw <shenyw@citycloud.com.cn>
2018-12-20 18:12:45 +0800

    style：修改日志格式

--- src/main/java/com/cucci/common/interceptors/LoggerAspect.java
+++ src/main/java/com/cucci/common/interceptors/LoggerAspect.java
@@ -9,7 +9,7 @@ import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Pointcut;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.stereotype.Service;
+import org.springframework.stereotype.Component;
 
 import java.util.Arrays;
 
@@ -19,10 +19,10 @@ import java.util.Arrays;
  * @author shenyw
  **/
 @Aspect
-@Service
+@Component
 public class LoggerAspect {
-
     private static final Logger logger = LoggerFactory.getLogger(LoggerAspect.class);
+
 
     @Pointcut("execution(com.cucci.common.vo.Result com.cucci.common.service.impl.*.*(..))")
     public void joinPoint() {
@@ -42,12 +42,13 @@ public class LoggerAspect {
             // TODO 可以将操作日志存储到数据中
             return result;
         } catch (Throwable e) {
-            String format = "Class name: %s. Method name：%s. Args is : %s. Error Message: %s";
-            String className = RegExpUtil.parse(jp.getThis().getClass().getName(), RegExpConstant.CLASS_NAME);
-            String methodName = jp.getSignature().getName();
-            String args = Arrays.toString(jp.getArgs());
-            String message = String.format(format, className, methodName, args, e.getMessage());
-            logger.error(message);
+            logger.error(
+                    "Class name: {}. Method name：{}. Args is : {}. \n Error Message: {}",
+                    RegExpUtil.parse(jp.getThis().getClass().getName(), RegExpConstant.CLASS_NAME),
+                    jp.getSignature().getName(),
+                    Arrays.toString(jp.getArgs()),
+                    e
+            );
             return Result.createError("系统异常");
         }
     }
