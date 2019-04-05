131070a3e78d568fc3b15e4d5143b9b3c84e6f95
shenyw <shenyw@citycloud.com.cn>
2019-04-05 10:30:56 +0800

    docs：添加注释信息

--- src/main/java/com/cucci/common/annotions/Group.java
+++ src/main/java/com/cucci/common/annotions/Group.java
@@ -4,7 +4,7 @@ import java.lang.annotation.*;
 
 /**
  * 用于校验表单的组
- *
+ * 该表单不能加Valid前缀不然的话会Spring会自动校验
  * @author shenyw@citycloud.com.cn
  **/
 @Target({ElementType.METHOD, ElementType.PARAMETER})
