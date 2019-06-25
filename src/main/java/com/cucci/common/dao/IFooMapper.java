8ce54426dbb4630b0649799c27197bcfc76f6d6f
shenyw <shenyw@citycloud.com.cn>
2019-06-25 15:00:32 +0800

    feat(多数据源): 验证多数据源

---
+++ src/main/java/com/cucci/common/dao/IFooMapper.java
@@ -0,0 +1,10 @@
+package com.cucci.common.dao;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.cucci.common.entity.Foo;
+
+/**
+ * @author shenyw
+ **/
+public interface IFooMapper extends BaseMapper<Foo> {
+}
