3a5d9fc1257454a8c7bdc6cca933b41f47274d05
shenyw <shenyw@citycloud.com.cn>
2020-06-19 15:13:37 +0800

    feat(统一异常处理): 增加统一异常处理的方式

---
+++ src/main/java/com/cucci/common/exceptions/ApplicationException.java
@@ -0,0 +1,20 @@
+package com.cucci.common.exceptions;
+
+/**
+ * 业务异常
+ *
+ * <br>
+ * 代码中能够察觉到的异常都应该转换成该异常，然后通过统一异常处理器进行处理
+ * </>
+ *
+ * @author shenyw@citycloud.com.cn
+ **/
+public class ApplicationException extends RuntimeException {
+
+    private static final long serialVersionUID = 982523153570073846L;
+
+    public ApplicationException(String message) {
+        super(message);
+    }
+
+}
