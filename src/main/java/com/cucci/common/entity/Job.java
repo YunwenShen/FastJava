68370060d06f74635ebb70d145033aa02bfa83f3
shenyw <shenyw@citycloud.com.cn>
2020-06-19 15:15:37 +0800

    fix(job): 修改覆盖异常

--- src/main/java/com/cucci/common/entity/Job.java
+++ src/main/java/com/cucci/common/entity/Job.java
@@ -3,8 +3,6 @@ package com.cucci.common.entity;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 
-import java.util.Map;
-
 /**
  * 普通任务 Cron
  *
@@ -18,9 +16,4 @@ public class Job extends BaseJob {
      * cron 表达式
      */
     private String cron;
-
-    /**
-     * 执行参数
-     */
-    private Map<String, Object> param;
 }
